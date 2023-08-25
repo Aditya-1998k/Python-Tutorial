440be7a5e665fe1a109be5b92e79ccf34587ce91
aditya <aditya98gupta@gmail.com>
2023-08-25 15:31:41 +0530

    learned escape character and comments

---
+++ Day5.py
@@ -0,0 +1,44 @@
+# writing comment is very important
+# in code it make our program understandable
+# it should be green color, errror in red
+# the above style is called <single-line> comment
+
+print('Above statement is comment')
+
+#write same statement in multiple line using / character
+
+print("here i want to write the same line in \
+more than one line so that i can avoid \
+more that 120 character in single line")
+
+
+#\n we can use to go to next line
+
+print("Hey Aditya ! \nHow are you?")
+
+
+"""
+this is the multiline comment
+which we can use as docstring in our function
+in zope it is mandotory to write docstring
+"""
+
+
+# print("Hey how are you"\nand this 
+#       viewer is also a good boy")\
+
+# to write above code correctly
+
+print("Hello i am a \"good boy\"\nand this\
+viewere is a good boy")
+      
+# output
+# Hello i am a "good boy"
+# and thisviewere is a good boy
+
+print("Hey",6,7, sep="~", end ="009\n")
+
+#multple element we need to print in single statement with seperator
+# output
+# Hey~6~7009
+
