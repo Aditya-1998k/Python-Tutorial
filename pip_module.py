ea2e11abaed6cb38f71ee5f8cc32f27bc1195780
aditya <aditya98gupta@gmail.com>
2023-08-25 09:28:29 +0530

    module

---
+++ pip_module.py
@@ -0,0 +1,20 @@
+#There are so many module that is avialable in python that we can use in our code
+#directly without write the same logic again in our code
+#pip is package manager
+
+# 1.Inbuilt module  -- hashlib
+# 2.External module --tensorflow
+
+#pip list    --will show all installed module
+#pip install panda
+#pip sckit-learn     --m/c learning module
+
+
+#python repl
+#read, evaluate, print, load
+#on consol type python3
+
+import hashlib
+import pandas   #pip install required
+
+
