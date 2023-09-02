aac2803b1fc377778bf668a690d2cf51d182f737
aditya <aditya98gupta@gmail.com>
2023-09-02 12:29:05 +0530

    Pdf merging task

--- Exercise8_mergePdfFile.py
+++ Exercise8_mergePdfFile.py
@@ -12,3 +12,15 @@ options , and passwords to pdf files.
 pypdf can retrieve text and metadata
 from pdf as well.
 """
+from PyPDF2 import PdfWriter
+import os
+merger = PdfWriter()
+dir="/home/ubuntu/Downloads"
+
+files = [file for file in os.listdir(dir) if file.endswith(".pdf")]
+
+for pdf in files:
+    merger.append(pdf)
+
+merger.write({dir})
+merger.close()
